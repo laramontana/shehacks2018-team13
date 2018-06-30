281ffbe297e1d3051990baf70f3f967525ec9266
laramontana <larabrightside@gmail.com>
2018-06-30 11:25:06 +1000

    DB connection in process...

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -2,11 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
 const actions_on_google_1 = require("actions-on-google");
+const admin = require("firebase-admin");
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 const app = actions_on_google_1.dialogflow();
+admin.initializeApp();
+// https://she-hacks-13-f2fc2.firebaseio.com/
 app.intent('start_feed', conv => {
     const time = new Date();
+    admin.database().ref('/she-hacks-13-f2fc2.firebaseio.com').push({ StartTime: time });
     conv.add(`Feed has started today at ${time.getUTCHours()}:${time.getUTCMinutes()}`);
 });
 app.intent('feed.stop', conv => {
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,18 @@
 import * as functions from 'firebase-functions';
 import { dialogflow } from 'actions-on-google';
+import * as admin from 'firebase-admin';
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
 const app = dialogflow();
+admin.initializeApp();
 
-app.intent('start_feed', conv => {
+// https://she-hacks-13-f2fc2.firebaseio.com/
+
+app.intent('feed.start', conv => {
     const time = new Date();
+    admin.database().ref('/she-hacks-13-f2fc2.firebaseio.com').push({StartTime: time})
     conv.add(`Feed has started today at ${time.getUTCHours()}:${time.getUTCMinutes()}`);
 });
 
