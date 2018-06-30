eb43b198cf22c86bfc44ebdfd1b565f645f822ab
Danqing Yin <angelayin@Danqings-MacBook.local>
2018-06-30 13:06:48 +1000

    db integration

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -4,10 +4,36 @@ const functions = require("firebase-functions");
 const actions_on_google_1 = require("actions-on-google");
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
+// Import database
+const admin = require("firebase-admin");
+const serviceAccount = require("../she-hacks-13-f2fc2-firebase-adminsdk-n5ubg-4e73d435df.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://she-hacks-13-f2fc2.firebaseio.com"
+});
+const db = admin.firestore();
+const docRef = db.collection('Time').doc('baby');
 const app = actions_on_google_1.dialogflow();
-app.intent('start_feed', conv => {
+app.intent('feed.start', conv => {
     const time = new Date();
-    conv.add(`hello world ${time.toString()}`);
+    docRef.set({
+        StartFeed: `${time.getHours()}:${time.getMinutes()}`,
+        StopFeed: 0
+    });
+    conv.add("Start feed");
+});
+app.intent('feed.stop', conv => {
+    const time = new Date();
+    docRef.update({
+        StopFeed: `${time.getHours()}:${time.getMinutes()}`
+    });
+    conv.add("Feed stopped");
 });
 exports.fulfillment = functions.https.onRequest(app);
+//var usersRef = ref.child("users");
+// usersRef.set({
+//   alanisawesome: {
+//     date_of_birth: "June 23, 1912",
+//     full_name: "Alan Turing"
+//   }
 //# sourceMappingURL=index.js.map
